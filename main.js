556601ba495b33dcee24038e4a444b2d2d8a8e4e
SenthilkumarNarayanan <124486793+SenthilkumarNarayanan@users.noreply.github.com>
2024-09-19 12:05:34 +0530

    Route structure

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 const express=require('express')
 const app=express();
 
-app.get('/',(req,res)=>{
-  res.json({msg:"Hello"});
-});
+//R-Read API for get all movies detail 
+app.get('/movies',(req,res)=>{
+
+})
+
+//C- Create API for create a new movie
+app.post('/movies',(req,res)=>{
+
+})
+
+//U- Update API for update existing movie details
+app.put('/movies/:id', (req,res)=>{
+
+})
+
+//D- Delete API for delete existing movie
+app.delete('/movies/:id',(req,res)=>{
+
+})
 const PORT=8000
 app.listen(PORT,()=>{
     console.log(`server is running on port http://localhost:${PORT}`)
